fed992d83b09b2a74d47c594a4a7f2dd033e9f11
JackMackinno1 <w0449149@campus.nscc.ca>
2022-11-22 20:50:11 -0400

    Finished connecting each page

--- Code/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Code/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,42 +1,82 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button placeOrderBtn, viewOrderBtn, changeOrderBtn, cancelOrderBtn;
+    Button placeOrderBtn, viewOrderBtn, changeOrderBtn, cancelOrderBtn, viewAllOrdersBtn;
 
+    TextView cruddyPizzaHeaderTextView, orderNumberLoopUpTextView, languageTextView;
+
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    Switch languageSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Connecting views
         placeOrderBtn = findViewById(R.id.placeOrderBtn);
         viewOrderBtn = findViewById(R.id.viewOrderBtn);
         changeOrderBtn = findViewById(R.id.changeOrderBtn);
         cancelOrderBtn = findViewById(R.id.cancelOrderBtn);
+        viewAllOrdersBtn = findViewById(R.id.viewAllOrdersBtn);
+        cruddyPizzaHeaderTextView = findViewById(R.id.cruddyPizzaHeaderTextView);
+        orderNumberLoopUpTextView = findViewById(R.id.orderNumberLookUpTextView);
+        languageTextView = findViewById(R.id.languageTextView);
+        languageSwitch = findViewById(R.id.languageSwitch);
 
 
-
-        placeOrderBtn.setOnClickListener(createOrder);
+        //Connecting listeners
         viewOrderBtn.setOnClickListener(searchOrder);
         changeOrderBtn.setOnClickListener(searchOrder);
-        cancelOrderBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, ViewAllOrders.class);
-                startActivity(i);
-            }
-        });
+        cancelOrderBtn.setOnClickListener(searchOrder);
+        placeOrderBtn.setOnClickListener(createOrder);
+        viewAllOrdersBtn.setOnClickListener(viewAllOrders);
+
+
 
 
     }
+
+    private View.OnClickListener searchOrder = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //Determine the action in the order look up
+            String action = "";
+
+            switch(v.getId()){
+                case R.id.viewOrderBtn:
+                    action = "VIEW";
+                    break;
+                case R.id.changeOrderBtn:
+                    action = "EDIT";
+                    break;
+                case R.id.cancelOrderBtn:
+                    action = "DELETE";
+                    break;
+            }
+
+            //Creating intent
+            Intent i = new Intent(MainActivity.this, OrderNumberLookUp.class);
+
+            Bundle extras = new Bundle();
+            extras.putString("ACTION", action);
+            i.putExtras(extras);
+            startActivity(i);
+
+        }
+    };
 
     private View.OnClickListener createOrder = new View.OnClickListener() {
         @Override
@@ -46,13 +86,14 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    private View.OnClickListener searchOrder = new View.OnClickListener() {
+    private View.OnClickListener viewAllOrders = new View.OnClickListener() {
         @Override
-        public void onClick(View view) {
-            Intent i = new Intent(MainActivity.this, OrderNumberLookUp.class);
+        public void onClick(View v) {
+            Intent i = new Intent(MainActivity.this, ViewAllOrders.class);
             startActivity(i);
         }
     };
+
 
 
 }
--- Code/app/src/main/java/com/example/myapplication/OrderNumberLookUp.java
+++ Code/app/src/main/java/com/example/myapplication/OrderNumberLookUp.java
@@ -2,13 +2,73 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class OrderNumberLookUp extends AppCompatActivity {
+
+    EditText orderNumberEditText;
+    Button orderNumberSubmitBtn;
+    TextView orderNumberLookUpTextView, cruddyPizzaHeaderTextView;
+    Bundle extras;
+
+    Intent i;
+    int numLookup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_number_look_up);
+
+        //Connecting views
+        orderNumberEditText = findViewById(R.id.orderNumberEditText);
+        orderNumberSubmitBtn = findViewById(R.id.orderNumberSubmitBtn);
+        orderNumberLookUpTextView = findViewById(R.id.orderNumberLookUpTextView);
+        cruddyPizzaHeaderTextView = findViewById(R.id.cruddyPizzaHeaderTextView);
+
+        orderNumberSubmitBtn.setOnClickListener(submitNumberLookUp);
+
+        extras = getIntent().getExtras();
+
+        if (extras != null)
+        {
+            switch(extras.getString("ACTION")) {
+                case "VIEW":
+                    i = new Intent(OrderNumberLookUp.this, ViewSpecificOrder.class);
+                    break;
+                case "DELETE":
+
+                    break;
+                case "EDIT":
+                    i = new Intent(OrderNumberLookUp.this, ChangeOrder.class);
+                    break;
+                default:
+                    i = new Intent(OrderNumberLookUp.this, MainActivity.class);
+                    break;
+            }
+
+        }
+
+
     }
+
+    View.OnClickListener submitNumberLookUp = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (orderNumberEditText.getText().toString().equals(""))
+            {
+                return;
+            }
+
+            numLookup = Integer.parseInt(orderNumberEditText.getText().toString());
+
+            i.putExtra("ORDERNUM", numLookup);
+            startActivity(i);
+        }
+    };
+
 }
