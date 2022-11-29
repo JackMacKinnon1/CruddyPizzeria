158edc29367eff0e1b4e7a036097c24da696bf07
w0449149 <w0449149@campus.nscc.ca>
2022-11-29 18:18:03 -0400

    Fucked around and found out did I ever

--- Code/app/src/main/java/com/example/myapplication/ViewAllOrders.java
+++ Code/app/src/main/java/com/example/myapplication/ViewAllOrders.java
@@ -1,12 +1,15 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
@@ -20,7 +23,6 @@ public class ViewAllOrders extends AppCompatActivity {
 
     TableRow orderRow;
     TextView cruddyPizzaHeaderTextView;
-    LinearLayout stk;
     TableLayout ll;
 
     SharedPreferences prefs;
@@ -35,48 +37,42 @@ public class ViewAllOrders extends AppCompatActivity {
 
 
         cruddyPizzaHeaderTextView = findViewById(R.id.cruddyPizzaHeaderTextView);
-
+        ll = findViewById(R.id.displayLinear);
 
         changeLanguage();
 
-        //init();
+        LayoutInflater inflator = LayoutInflater.from(this);
+
+        for (int i = 0; i < 10; i++) {
+
+            View tr = inflator.inflate(R.layout.order_cell, null, false);
+            ll.addView(tr);
 
 
-
-
-    }
-
-    public void init() {
-        ll = (TableLayout) findViewById(R.id.displayLinear);
-        ll.removeAllViews();
-
-        for (int i = 0; i < 2; i++) {
-//            TableRow row = (TableRow) findViewById(R.id.display_row);
-//            checkBox = new CheckBox(this);
-//            tv = new TextView(this);
-//            addBtn = new ImageButton(this);
-//            addBtn.setImageResource(R.drawable.add);
-//            minusBtn = new ImageButton(this);
-//            minusBtn.setImageResource(R.drawable.minus);
-//            qty = new TextView(this);
-//            checkBox.setText("hello");
-//            qty.setText("10");
-//            row.addView(checkBox);
-//            row.addView(minusBtn);
-//            row.addView(qty);
-//            row.addView(addBtn);
-//            ll.addView(row, i);
-
-            TableRow row = new TableRow(this);
-            row.setBackgroundResource(R.drawable.table_background);
-            row.setMinimumWidth(200);
-            row.setMinimumHeight(70);
-
-            ll.addView(row);
+//            ll.addView(row);
 
         }
 
+
+
+
     }
+//
+//    public void init() {
+//
+//        LayoutInflater inflator = LayoutInflater.from(this);
+//
+//        for (int i = 0; i < 2; i++) {
+//
+//            View tr = inflator.inflate(R.layout.order_cell, null, false);
+//            ll.addView(tr);
+//
+//
+////            ll.addView(row);
+//
+//        }
+//
+//    }
 
     private void changeLanguage() {
         prefs = getSharedPreferences("LanguageValue", MODE_PRIVATE);
